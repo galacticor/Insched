3a58c1375c96d863249507188e1a1f08b84ccef8
Marcia Nadin <nadinmarcia@gmail.com>
2021-04-30 14:16:54 +0700

    add more code for showCalendar.java

--- src/main/java/com/bot/insched/command/ShowCalendarCommand.java
+++ src/main/java/com/bot/insched/command/ShowCalendarCommand.java
@@ -1,17 +1,22 @@
 package com.bot.insched.command;
 
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
+import com.bot.insched.service.EventService;
+import net.dv8tion.jda.api.events.Event;
 
 public class ShowCalendarCommand implements Command{
-
-
+    private Event event;
+    private EventService eventService;
     @Override
     public void execute(String[] args, PrivateMessageReceivedEvent event) {
+        event.getAuthor().openPrivateChannel().queue(privateChannel -> {
+            privateChannel.sendMessage("Ini adalah fitur untuk melihat Calendar mu !").queue();
+        });
 
     }
 
     @Override
     public String getCommand() {
-        return null;
+        return "showCalendar";
     }
 }
