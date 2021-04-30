68ae3d274a59cdfdb92500686ebdb15aa24fcb0c
unknown <muadzaskar19@gmail.com>
2021-04-30 13:44:50 +0700

    add event service impl

--- src/main/java/com/bot/insched/service/EventService.java
+++ src/main/java/com/bot/insched/service/EventService.java
@@ -1,4 +1,11 @@
 package com.bot.insched.service;
 
+import com.bot.insched.model.Event;
+
 public interface EventService {
+    Event createEvent(Event event);
+    void deleteEvent(String idEvent);
+    Event updateEvent(String idEvent,Event event);
+    Event getEvent(String idEvent);
+
 }
--- src/main/java/com/bot/insched/service/EventServiceImpl.java
+++ src/main/java/com/bot/insched/service/EventServiceImpl.java
@@ -1,4 +1,33 @@
 package com.bot.insched.service;
 
+import com.bot.insched.model.Event;
+import com.bot.insched.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class EventServiceImpl implements EventService{
+    @Autowired
+    private EventRepository eventRepository;
+    @Override
+    public Event createEvent(Event event) {
+        eventRepository.save(event);
+        return event;
+    }
+
+    @Override
+    public Event getEvent(String idEvent) {
+        return eventRepository.findByIdEvent(idEvent);
+    }
+
+    @Override
+    public Event updateEvent(String idEvent,Event event) {
+        event.setIdEvent(idEvent);
+        eventRepository.save(event);
+        return event;
+    }
+
+    @Override
+    public void deleteEvent(String idEvent) {
+        Event event = eventRepository.findByIdEvent(idEvent);
+        eventRepository.delete(event);
+    }
 }
