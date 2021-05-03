74b9c2e2b8693a98b585c05698cb22231800195c
widyantohadi <widyantohadi@gmail.com>
2021-05-03 09:47:39 +0700

    add oa

--- src/main/java/com/bot/insched/google/GoogleAPIManager.java
+++ src/main/java/com/bot/insched/google/GoogleAPIManager.java
@@ -5,11 +5,17 @@ import com.bot.insched.google.credentials.JPADataStoreFactory;
 import com.bot.insched.repository.DiscordUserRepository;
 
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.CredentialRefreshListener;
+import com.google.api.client.auth.oauth2.DataStoreCredentialRefreshListener;
+import com.google.api.client.auth.oauth2.StoredCredential;
+import com.google.api.client.auth.oauth2.TokenErrorResponse;
+import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
@@ -17,6 +23,9 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.util.store.DataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.oauth2.Oauth2;
+import com.google.api.services.oauth2.Oauth2Scopes;
+import com.google.api.services.oauth2.model.Userinfoplus;
 
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +36,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -35,7 +44,7 @@ import java.util.List;
 public class GoogleAPIManager {
 	private static final String APPLICATION_NAME = "Insched (Instant Scheduler)";
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR);
+    private static final List<String> SCOPES = Arrays.asList(CalendarScopes.CALENDAR, Oauth2Scopes.USERINFO_EMAIL);
 
     private static final String CLIENT_SECRET = "oksfFIk_VzNxb9G08Iup7_1U";
     private static final String CLIENT_ID = "38754712208-el2lrejbff3mineg0sc0cbiimiqbj349.apps.googleusercontent.com";
@@ -67,7 +76,7 @@ public class GoogleAPIManager {
 					.setAccessType("offline")
 					.build();	
 		} catch (Exception e){
-			log.error("Error when configurate");
+			log.error("------ Error when configurate");
 		}
 		
 	}
@@ -76,19 +85,11 @@ public class GoogleAPIManager {
 		String redirectTo = flow.newAuthorizationUrl()
 				.setRedirectUri(REDIRECT_URI)
 				.build();
-		// GoogleAuthorizationCodeRequestUrl urlBuilder =
-		//     new GoogleAuthorizationCodeRequestUrl( CLIENT_ID, REDIRECT_URI, SCOPES)
-		//     .setAccessType("offline");
 	  	return redirectTo;
 	}
 
 	public boolean authToken(String userId, String code) {
 		try {
-			// GoogleTokenResponse tokenResponse = new GoogleAuthorizationCodeTokenRequest(
-   //            httpTransport,
-   //            JSON_FACTORY,
-   //            "https://oauth2.googleapis.com/token",
-   //            CLIENT_ID, CLIENT_SECRET, code, REDIRECT_URI).execute();
 			GoogleTokenResponse googleResponse = flow.newTokenRequest(code)
 					.setRedirectUri(REDIRECT_URI)
 					.execute();
@@ -97,11 +98,78 @@ public class GoogleAPIManager {
 
 			return true;
 		} catch (Exception e){
-			log.warn("------Error when save credential for user {} : {}", userId, e);
+			log.warn("------ Error when save credential for user {} : {}", userId, e);
 			return false;
 		}
-		
 	}
 
+	public GoogleCredential getCredential(String userId) {
+		try{
+			Credential storedCredential = flow.loadCredential(userId);
+			if (storedCredential == null) {
+				log.warn("------ Couldn't retrieve Google credential for user {}", userId);
+				return null;
+			}
+			GoogleCredential credential = new GoogleCredential.Builder()
+					.setTransport(httpTransport)
+					.setJsonFactory(JSON_FACTORY)
+					.setClientSecrets(CLIENT_ID, CLIENT_SECRET)
+					.addRefreshListener(new CredentialRefreshListener() {
+						public void onTokenErrorResponse(Credential credential, TokenErrorResponse tokenErrorResponse) {
+							log.error("OAuth token refresh error: {}", tokenErrorResponse);
+						}
+
+						public void onTokenResponse(Credential credential, TokenResponse tokenResponse) {
+							log.debug("OAuth token was refreshed");
+						}
+					})
+					.addRefreshListener(new DataStoreCredentialRefreshListener(userId, flow.getCredentialDataStore()))
+					.build();
+
+			credential.setRefreshToken(storedCredential.getRefreshToken());
+			credential.setAccessToken(storedCredential.getAccessToken());
+			log.debug("Credential: Get Credential for user : {}", credential.getServiceAccountUser());
+
+			return credential;
+		} catch (Exception e) {
+			log.warn("Credential: Error while refreshing or saving the token for user {} : {}", userId, e.getMessage());
+		}
+
+		return null;
+	}
+
+	public Calendar getCalendarService (String userId){
+		GoogleCredential credential = getCredential(userId);
+		if (credential == null) {
+			return null;
+		}
+
+		Calendar service = new Calendar.Builder(httpTransport, JSON_FACTORY, credential)
+	            .setApplicationName(APPLICATION_NAME)
+	            .build();
+
+	    return service;
+	}
+
+	public Oauth2 getOauth2Service (String userId) {
+		GoogleCredential credential = getCredential(userId);
+		Oauth2 service = new Oauth2.Builder(httpTransport, JSON_FACTORY, credential)
+				.setApplicationName("Oauth2")
+				.build();
+
+		return service;
+	}
+
+	public Userinfoplus getUserInfo(String userId) {
+		Oauth2 oauth2 = getOauth2Service(userId);
+		try {
+			Userinfoplus userinfo = oauth2.userinfo().get().execute();	
+			return userinfo;
+		} catch (Exception e){
+			log.warn("Userinfo: Error while execute userinfo {} : {}", userId, e.getMessage());
+		}
+		
+		return null;
+	}
 
 }
