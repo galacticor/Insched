bfccccb3944e9c372ef8d51f0fa286ee2420c4e7
Marcia Nadin <nadinmarcia@gmail.com>
2021-05-06 16:40:44 +0700

    benerin showCalendarCommand yang disuru sonarlint

--- src/main/java/com/bot/insched/discord/command/ShowCalendarCommand.java
+++ src/main/java/com/bot/insched/discord/command/ShowCalendarCommand.java
@@ -1,14 +1,13 @@
 package com.bot.insched.discord.command;
 
-import com.bot.insched.service.GoogleService;
 import com.bot.insched.service.ShowCalendarService;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import com.bot.insched.service.EventService;
 import net.dv8tion.jda.api.events.Event;
 
 public class ShowCalendarCommand implements Command{
-    private Event event;
-    private EventService eventService;
+    Event event;
+    EventService eventService;
     private ShowCalendarService service;
 
     public ShowCalendarCommand(ShowCalendarService service){
@@ -23,9 +22,9 @@ public class ShowCalendarCommand implements Command{
         String userId = event.getMessage().getAuthor().getId();
         if(service.getCalService(userId)!=null) {
             String reply = service.getCalService(userId);
-            event.getAuthor().openPrivateChannel().queue(privateChannel -> {
+            event.getAuthor().openPrivateChannel().queue(privateChannel ->
                 privateChannel.sendMessage(reply).queue();
-            });
+            );
         }
         else sendPrivateMessage("Ups.. kalender kamu belum ada, masukkan perintah !login", event);
 
@@ -36,7 +35,7 @@ public class ShowCalendarCommand implements Command{
         event.getAuthor().openPrivateChannel().queue(privateChannel -> {
             privateChannel.sendMessage(response).queue();
         });
-    };
+    }
 
 
     @Override
