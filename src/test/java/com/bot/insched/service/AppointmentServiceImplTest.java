06bdd6e5f01d11c622025a173df67a2592df15d1
Fikri Akmal <fikriakmal453@gmail.com>
2021-05-07 08:11:21 +0700

    add new test

--- src/test/java/com/bot/insched/service/AppointmentServiceImplTest.java
+++ src/test/java/com/bot/insched/service/AppointmentServiceImplTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,6 +21,8 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,7 +32,7 @@ public class AppointmentServiceImplTest {
     private AppointmentRepository appointmentRepository;
 
     @Mock
-    private DiscordUserRepository discordUserRepository;
+    DiscordUserService discordUserService;
 
     @Mock
     private EventService eventService;
@@ -63,13 +64,66 @@ public class AppointmentServiceImplTest {
     @Test
     public void testGetUserToken() {
         Appointment app = new Appointment();
-        app.setIdAppointment(UUID.fromString("123"));
+        app.setIdAppointment(UUID.fromString("70bab1b3-7e8e-4b3e-84b5-e95901e1925d"));
         user.setAppointment(app);
 
         String response = appointmentService.getUserToken(user);
-        assertEquals(response, "123");
+        assertEquals(response, "70bab1b3-7e8e-4b3e-84b5-e95901e1925d");
     }
 
+    @Test
+    public void testGetUserTokenNull() {
+        String response = appointmentService.getUserToken(user);
+        assertEquals(response, anyString());
+    }
+
+    @Test
+    public void testCreateSlotNoUser() {
+        lenient().when(discordUserService.findByUserId(any())).thenReturn(null);
+        assertThrows(Exception.class , () -> {
+            appointmentService.createSlot("aa","2021-05-03T15:30:00",1,1,"123");
+        });
+    }
+
+    @Test
+    public void testCreateAlreadyExists() {
+        lenient().when(discordUserService.findByUserId(anyString())).thenReturn(user);
+
+
+        List<Event> userAppointment = new ArrayList<>();
+        Event event = new Event("2021-03-05T15:30", 30, 2, "dummy");
+        userAppointment.add(event);
+        user.setAppointment(appointment);
+        user.getAppointment().setListEvent(userAppointment);
+
+        assertThrows(Exception.class, () -> {
+            appointmentService.createSlot("dummy", "2021-03-05T15:30", 30,  2, "123");
+        });
+    }
+
+    @Test
+    public void testCreateSlotSuccess() throws Exception{
+        lenient().when(discordUserService.findByUserId(anyString())).thenReturn(user);
+        List<Event> userAppointment = new ArrayList<>();
+        user.setAppointment(appointment);
+        user.getAppointment().setListEvent(userAppointment);
+
+        when(appointmentRepository.findAppointmentByOwner(any()))
+                .thenReturn(appointment);
+
+        String res = appointmentService.createSlot("aa","2021-05-03T15:30:00",1,1,"123");
+        assertEquals(res, "Slot berhasil dibuat!");
+    }
+
+    @Test
+    public void testGetAllAppointment() {
+        when(discordUserService.findByUserId(any())).thenReturn(user);
+        when(appointmentRepository.findAppointmentByOwner(any())).thenReturn(appointment);
+        appointment.setListEvent(new ArrayList<>());
+        List<Event> expected = appointment.getListEvent();
+        assertEquals(expected, appointmentService.getAllAppointment("123"));
+
+    }
 
 
 
