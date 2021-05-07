5db7de1190a57d2fcfdb0e7fd4f771832b18dc8a
Fikri Akmal <fikriakmal453@gmail.com>
2021-05-07 18:32:32 +0700

    edit google api manager

--- src/main/java/com/bot/insched/google/GoogleAPIManager.java
+++ src/main/java/com/bot/insched/google/GoogleAPIManager.java
@@ -21,6 +21,7 @@ import java.util.Arrays;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
@@ -32,18 +33,13 @@ public class GoogleAPIManager {
     private static final List<String> SCOPES =
         Arrays.asList(CalendarScopes.CALENDAR, Oauth2Scopes.USERINFO_EMAIL);
 
-    //    @Value("${client_secret}")
-//    private static String CLIENT_SECRET;
-    //
-//    @Value("${client_id}")
-//    private static String CLIENT_ID;
-
-    //    @Value("%{redirect_uri}")
-//    private static String REDIRECT_URI;
-    private static final String CLIENT_SECRET = "oksfFIk_VzNxb9G08Iup7_1U";
-    private static final String CLIENT_ID =
-        "38754712208-el2lrejbff3mineg0sc0cbiimiqbj349.apps.googleusercontent.com";
-    private static final String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
+    private static String CLIENT_SECRET;
+    private static String CLIENT_ID;
+    private static String REDIRECT_URI;
+//    private static final String CLIENT_SECRET = "oksfFIk_VzNxb9G08Iup7_1U";
+//    private static final String CLIENT_ID =
+//        "38754712208-el2lrejbff3mineg0sc0cbiimiqbj349.apps.googleusercontent.com";
+//    private static final String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
 
 
     private DiscordUserRepository userRepo;
@@ -52,26 +48,26 @@ public class GoogleAPIManager {
     private GoogleAuthorizationCodeFlow flow;
     private HttpTransport httpTransport;
 
-//    @Autowired
-//    public GoogleAPIManager(
-//        DiscordUserRepository repository,
-//        Builder builder,
-//        @Value("${client_id}") String clientId,
-//        @Value("${client_secret}") String clientSecret,
-//        @Value("${redirect_uri}") String redirectUri) {
-//        this.userRepo = repository;
-//        this.builder = builder;
-//        init();
-//    }
-
     @Autowired
     public GoogleAPIManager(
         DiscordUserRepository repository,
-        Builder builder) {
+        Builder builder,
+        @Value("${client_id}") String clientId,
+        @Value("${client_secret}") String clientSecret,
+        @Value("${redirect_uri}") String redirectUri) {
         this.userRepo = repository;
         this.builder = builder;
         init();
     }
+
+//    @Autowired
+//    public GoogleAPIManager(
+//        DiscordUserRepository repository,
+//        Builder builder) {
+//        this.userRepo = repository;
+//        this.builder = builder;
+//        init();
+//    }
 
     public void init() {
         try {
