d093b3a79bf92a3f774de7c7698e48126bf20c32
Fikri Akmal <fikriakmal453@gmail.com>
2021-05-07 18:29:36 +0700

    enhance checkstyle

--- src/main/java/com/bot/insched/google/Builder.java
+++ src/main/java/com/bot/insched/google/Builder.java
@@ -3,10 +3,10 @@ package com.bot.insched.google;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.services.calendar.Calendar;
-import com.google.api.services.oauth2.Oauth2;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.oauth2.Oauth2;
 import java.util.List;
 import org.springframework.stereotype.Component;
 
@@ -18,11 +18,14 @@ public class Builder {
         return new GoogleCredential.Builder();
     }
 
-    public Calendar.Builder getCalendarBuilder(HttpTransport httpTransport, JsonFactory jsonFactory, GoogleCredential credential) {
+    public Calendar.Builder getCalendarBuilder(HttpTransport httpTransport,
+                                               JsonFactory jsonFactory,
+                                               GoogleCredential credential) {
         return new Calendar.Builder(httpTransport, jsonFactory, credential);
     }
 
-    public Oauth2.Builder getOauth2Builder(HttpTransport httpTransport, JsonFactory jsonFactory, GoogleCredential credential) {
+    public Oauth2.Builder getOauth2Builder(HttpTransport httpTransport, JsonFactory jsonFactory,
+                                           GoogleCredential credential) {
         return new Oauth2.Builder(httpTransport, jsonFactory, credential);
     }
 
@@ -32,6 +35,7 @@ public class Builder {
         String client_id,
         String client_secret,
         List<String> scopes) {
-        return new GoogleAuthorizationCodeFlow.Builder(httpTransport, jsonFactory, client_id, client_secret, scopes);
+        return new GoogleAuthorizationCodeFlow.Builder(httpTransport, jsonFactory, client_id,
+            client_secret, scopes);
     }
 }
