2c96d0dec6d52e515b12ad133453d4228c025b94
Darian Texanditama <dariantexanditama@protonmail.ch>
2021-05-21 16:13:49 +0700

    implement delete booking command

---
+++ src/main/java/com/bot/insched/discord/command/DeleteBookAppointmentCommand.java
@@ -0,0 +1,64 @@
+package com.bot.insched.discord.command;
+
+import com.bot.insched.discord.util.InschedEmbed;
+import com.bot.insched.discord.util.MessageSender;
+import com.bot.insched.service.BookingAppointmentService;
+import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
+
+public class DeleteBookAppointmentCommand implements Command{
+
+    private BookingAppointmentService bookingAppointmentService;
+
+    private MessageSender sender = MessageSender.getInstance();
+
+    public DeleteBookAppointmentCommand(BookingAppointmentService bookingAppointmentService) {
+        this.bookingAppointmentService = bookingAppointmentService;
+    }
+
+    @Override
+    public void execute(String[] args, PrivateMessageReceivedEvent event) {
+        InschedEmbed embed = new InschedEmbed();
+        embed.setTitle("Unbook Appointment");
+        embed.setDescription("Selamat Datang di fitur Unbook Appointment");
+
+        event.getAuthor().openPrivateChannel().queue(privateChannel -> {
+            privateChannel.sendMessage(embed.build()).queue();
+        });
+
+        try {
+            if (args[0].equalsIgnoreCase("help")) {
+                sender.sendPrivateMessage(getHelp(), event);
+            } else {
+                String response = deletionHandler(args, event);
+                sender.sendPrivateMessage(response, event);
+            }
+        } catch (IndexOutOfBoundsException e) {
+            sender.sendPrivateMessage(
+                    "Masukkan argumen yang sesuai!\n" +
+                            "Penggunaan: !unbookAppointment <token_event>\n" +
+                            "Help: !unbookAppointment help", event);
+        } catch (Exception e) {
+            sender.sendPrivateMessage(e.getMessage(), event);
+        }
+    }
+
+    @Override
+    public String getCommand() {
+        return "unbookAppointment";
+    }
+
+    @Override
+    public String getHelp() {
+        return "Digunakan untuk menghapus booking pada slot event dalam sebuah appointment.\n" +
+                "Penggunaan: !unbookAppointment <token_event>\n" +
+                "Contoh: !unbookAppointment e79e7cf1-0b8c-48db-a05b-baafcb5953d2";
+    }
+
+    public String deletionHandler(String[] args, PrivateMessageReceivedEvent event) throws Exception {
+
+        String userId = event.getAuthor().getId();
+        String token = args[0];
+
+        return bookingAppointmentService.deleteBooking(userId, token);
+    }
+}
