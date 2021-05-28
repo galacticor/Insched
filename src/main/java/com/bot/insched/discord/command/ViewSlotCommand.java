ec58109b85ab46a5aef2f0797666b4f92ea5dfd2
Darian Texanditama <dariantexanditama@protonmail.ch>
2021-05-28 18:42:00 +0700

    Finish implement view host slot command

--- src/main/java/com/bot/insched/discord/command/ViewSlotCommand.java
+++ src/main/java/com/bot/insched/discord/command/ViewSlotCommand.java
@@ -1,5 +1,6 @@
 package com.bot.insched.discord.command;
 
+import com.bot.insched.discord.util.InschedEmbed;
 import com.bot.insched.discord.util.MessageSender;
 import com.bot.insched.model.Event;
 import com.bot.insched.service.BookingAppointmentService;
@@ -20,7 +21,22 @@ public class ViewSlotCommand implements Command{
 
     @Override
     public void execute(String[] args, PrivateMessageReceivedEvent event) {
-
+        try {
+            if (args[0].equalsIgnoreCase("help")) {
+                sender.sendPrivateMessage(getHelp(), event);
+            } else {
+                List<Event> eventList = viewHandler(args, event);
+                InschedEmbed response = embedHandler(eventList, args[0]);
+                sender.sendPrivateMessage(response.build(), event);
+            }
+        } catch (IndexOutOfBoundsException e) {
+            sender.sendPrivateMessage(
+                    "Masukkan argumen yang sesuai!\n" +
+                            "Penggunaan: !viewSlot <host-token>\n" +
+                            "Help: !viewSlot help", event);
+        } catch (Exception e) {
+            sender.sendPrivateMessage(e.getMessage(), event);
+        }
     }
 
     @Override
@@ -31,7 +47,7 @@ public class ViewSlotCommand implements Command{
     @Override
     public String getHelp() {
         return "Menampilkan semua slot milik Host\n" +
-                "!viewSlot <host-token>\n" +
+                "Penggunaan: !viewSlot <host-token>\n" +
                 "Contoh: !viewSlot f2da393a-7ef2-4fe9-979e-ea3d76adc7ea";
     }
 
@@ -41,6 +57,21 @@ public class ViewSlotCommand implements Command{
         String token = args[0];
 
         return bookingAppointmentService.viewHostBookingSlots(userId, token);
+    }
+
+    public InschedEmbed embedHandler(List<Event> eventList, String appointmentToken) throws Exception {
+        InschedEmbed embed = new InschedEmbed();
+        embed.setTitle("Appointment List");
+        embed.setDescription("Token: " + appointmentToken);
+
+        for (Event event : eventList) {
+                String desc = event.getDescription();
+                String eventToken = "Token: " + event.getIdEvent().toString() + "\n";
+                String time = event.getWaktu() + "\n";
+                String bookingStatus = event.getStatusBooking();
+                embed.addField(desc, time + eventToken + bookingStatus, false);
+        }
+        return embed;
     }
 
 }
