00c718fc0f5faa6ed8adb181f737272dc7ac62e7
Fikri Akmal <fikriakmal453@gmail.com>
2021-06-07 12:45:25 +0700

    fix doubled appointment

--- src/main/java/com/bot/insched/service/BookingAppointmentServiceImpl.java
+++ src/main/java/com/bot/insched/service/BookingAppointmentServiceImpl.java
@@ -14,6 +14,7 @@ import com.bot.insched.repository.EventRepository;
 import java.util.*;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -57,7 +58,6 @@ public class BookingAppointmentServiceImpl implements BookingAppointmentService 
         eventRepository.save(event);
         listEvent.add(event);
         attendee.setListEvent(listEvent);
-
         discordUserRepository.save(attendee);
 
         return "Booking slot event telah dibuat!";
@@ -104,8 +104,10 @@ public class BookingAppointmentServiceImpl implements BookingAppointmentService 
             throw new ObjectNotFoundException("Appointment tidak ditemukan!");
         }
 
-        List<Event> eventList = appointment.getListEvent();
+        Set<Event> eventSet = new HashSet<>(appointment.getListEvent());
+        List<Event> eventList = eventSet.stream().collect(Collectors.toList());
         Collections.sort(eventList);
+
         return eventList;
     }
 
